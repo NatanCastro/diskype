036de3734f093a1b2a66cdba8595e984020f06af
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-12-10 14:29:17 -0300

    init commit

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="container">
+    <img id="logo" src="/logo.svg" alt="" />
+    <div id="profile"></div>
+    <div id="chats">
+      <h1>CHATS</h1>
+      <nav></nav>
+    </div>
+  </div>
+</template>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+}
+
+#logo {
+  width: 100%;
+  height: 50px;
+  background-color: #fe43ef33;
+}
+
+#profile {
+  width: 100%;
+  height: 250px;
+  background-color: #ecf3;
+}
+
+#chats {
+  flex: 1;
+  background-color: #ea43;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Sidebar from '@/components/Sidebar.vue'
+</script>
+
+<template>
+  <main>
+    <Sidebar />
+  </main>
+</template>
+<style scoped>
+main {
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 2fr 10fr;
+  background-color: #0f0f0ffe;
+}
+</style>
